dd00cbaa15bbb6e63df1bc5d8a01bd97d7a09684
srihari <srihari.r@taski.in>
2023-08-02 08:12:07 +0530

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+      <div>
+        <a href="mailto:letterstonithishkumar@gmail.com">
+          <FiMail />
+        </a>
+      </div>
+      <div>
+        <a href='https://www.linkedin.com/in/abinithishkumarprabakaran/' target='_blank' rel='noreferrer'>
+          <FaLinkedinIn />
+        </a>
+      </div>
+      <div>
+        <a href='https://www.instagram.com/abinithishkumar/' target='_blank' rel='noreferrer'>
+          <BsInstagram />
+        </a>
+      </div>
+      <div>
+        <a href='https://github.com/Abinithishkumarprabakaran' target='_blank' rel='noreferrer'>
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a href='https://twitter.com/nithishkumar_pr?t=v8nFiMEWqVfXK_7FYB7huQ&s=35' target='_blank' rel='noreferrer'>
+          <BsTwitter />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default SocialMedia
---
+++ src/constants/images.js
@@ -0,0 +1,48 @@
+import css from '../assets/css.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import ak_logo from '../assets/ak_logo.png';
+
+import mongodb from '../assets/mongodb.svg';
+import express from '../assets/express.svg';
+import react from '../assets/react.svg';
+import vscode from '../assets/vscode.png';
+
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import logo from '../assets/logo.png';
+export default {
+  css,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+
+  profile,
+  circle,
+
+  ak_logo,
+  mongodb,
+  express,
+  vscode,
+
+  email,
+  mobile,
+  logo
+};
